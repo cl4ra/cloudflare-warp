0537fed4bfe4bb7f2c5781385845b0edd4da93ea
cl4ra <45875254+cl4ra@users.noreply.github.com>
2020-10-06 20:41:42 +0800

    Update cloudflare-warp.py

--- cloudflare-warp.py
+++ cloudflare-warp.py
@@ -23,32 +23,49 @@ def genString(stringLength):
     return ''.join(random.choice(letters) for i in range(stringLength))
 
 
-url = f'https://api.cloudflareclient.com/v0a{digitString(3)}/reg'
-def run():
-	try:
-		install_id = genString(22)
-		body = {"key": "{}=".format(genString(43)),
-				"install_id": install_id,
-				"fcm_token": "{}:APA91b{}".format(install_id, genString(134)),
-				"referrer": referrer,
-				"warp_enabled": False,
-				"tos": datetime.datetime.now().isoformat()[:-3] + "+02:00",
-				"type": "Android",
-				"locale": "es_ES"}
-		data = json.dumps(body).encode('utf8')
-		headers = {'Content-Type': 'application/json; charset=UTF-8',
-					'Host': 'api.cloudflareclient.com',
-					'Connection': 'Keep-Alive',
-					'Accept-Encoding': 'gzip',
-					'User-Agent': 'okhttp/3.12.1'
-					}
-		req         = urllib.request.Request(url, data, headers)
-		response    = urllib.request.urlopen(req)
-		status_code = response.getcode()	
-		return status_code
-	except Exception as error:
-		print(error)	
+url = 'https://api.cloudflareclient.com/v0a745/reg'
 
+def run():
+
+    install_id = genString(11)
+
+    body = {"key": "{}=".format(genString(42)),
+
+            "install_id": install_id,
+
+            "fcm_token": "{}:APA91b{}".format(install_id, genString(134)),
+
+            "referrer": referrer,
+
+            "warp_enabled": False,
+
+            "tos": datetime.datetime.now().isoformat()[:-3] + "+08:00",
+
+            "type": "Android",
+
+            "locale": "zh-CN"}
+
+    bodyString = json.dumps(body)
+
+    headers = {'Content-Type': 'application/json; charset=UTF-8',
+
+               'Host': 'api.cloudflareclient.com',
+
+               'Connection': 'Keep-Alive',
+
+               'Accept-Encoding': 'gzip',
+
+               'User-Agent': 'okhttp/3.12.1'
+
+               }
+
+    r = requests.post(url, data=bodyString, headers=headers)
+
+    
+
+    return r
+
+c = 1
 
 while True:
 
@@ -56,11 +73,10 @@ while True:
 
     if result.status_code == 200:
 
-        print(c,"GB added successfully!")
+        print(c,"+1GB")
 
         c = c + 1
 
     else:
 
         time.sleep(1)
-
